45fc98fded0e61cb121d100de555c24487cb055d
Paul Gaida <pc.gaida@gmail.com>
2021-11-29 21:24:45 +0100

    Use numeric quantifiers.

---
+++ regex/escape-sequences-and-quantifiers.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => 'all';
+
+$| = 1;
+
+sub main {
+    my ($self) = @_;
+
+    # examples escape sequences:
+    # \d => digit
+    # \s => space
+    # \S => non-space
+    # \w => alphanumeric
+
+    my $text = 'I am 117 years old tomorrow.';
+    if($text =~ /(\d+\s\S*)/) {
+        print("$1\n");
+    }
+
+    # examples numeric quantifiers
+    # * => zero or more preceding characters
+    # *? => zero or more preceding characters, as few as possible
+    # + => one or more preceding characters, as many as possible
+    # +? => one or more preceding characters, as few as possible
+    # {3} => exactly three of the preceding characters or sequences
+    # {3,6} => between 3 and 6 of the preceding (inclusive)
+    # {3,} => at least three (inclusive)
+
+    my $code = 'DE$2342555';
+    if($code =~ /(DE\$\d{5,10})/) {
+        print("$1\n");
+    }
+}
+
+main();
