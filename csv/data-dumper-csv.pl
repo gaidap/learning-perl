caaade7dcdc825609160b355d01f31d573676a95
Paul Gaida <pc.gaida@gmail.com>
2021-12-23 20:23:24 +0100

    Read a csv file and print its values with data dumper and trim outputs.

---
+++ csv/data-dumper-csv.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => 'all';
+
+use Data::Dumper;
+
+$| = 1;
+
+sub main {
+    my ($self) = @_;
+    my $input = 'test.csv';
+
+    unless (open(INPUT, $input)) {
+        die("\nCannot open $input");
+    }
+
+    <INPUT>; # Skip the first line
+    while (<INPUT>) {
+        # Remove spaces and newlines from beginning and end of the line
+        chomp $_;
+
+        my @values = split(/\s*,\s*/, $_); # use regex to trim spaces on split
+
+        print Dumper(@values); # Print data from array to console.
+    }
+
+    close(INPUT);
+}
+
+main();
---
+++ csv/join-and-split-csv.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => 'all';
+
+$| = 1;
+
+sub main {
+    my ($self) = @_;
+    my $input = 'test.csv';
+
+    unless (open(INPUT, $input)) {
+        die("\nCannot open $input");
+    }
+
+    <INPUT>; # Skip the first line
+    while (<INPUT>) {
+        # Read one line until it returns undef
+        my @values = split(",", $_); # $_ is the current line
+        print join('|', @values);
+    }
+
+    close(INPUT);
+}
+
+main();
