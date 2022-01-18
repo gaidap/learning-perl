2e7e0b33e841555a6a9995ebadc4e05daa5b4f64
Paul Gaida <pc.gaida@gmail.com>
2022-01-18 11:26:06 +0100

    Pushing to array in Perl.

--- arrays/arrays.pl
+++ arrays/arrays.pl
@@ -6,17 +6,29 @@ $|=1; # Deactivate output buffering
 
 sub main {
     my ($self) = @_;
-    my $vimrc = '/home/pac/vimrc';
-    my $bashrc = '/home/pac/.bashrc';
-    my $ideavimrc = '/home/pac/.ideavimrc';
-    my @files = ($vimrc, $bashrc, $ideavimrc);
-    foreach my $file (@files) {
-        if (-f $file) {
-            print("File $file exists.\n");
-        }
-        else {
-            print("File $file not exists.")
-        }
+
+    my $testFile = 'test.csv';
+
+    unless (open(INPUT, $testFile)) {
+        die("Failed to open file <<$testFile>>.\n");
     }
 
+    <INPUT>;
+
+    my @lines; # Perl initializes arrays as empty on default.
+
+    while (my $line = <INPUT>) {
+        chomp($line);
+        my @values = split(/\s*,\s*/, $line);
+        # $lines[$count] = $line; One way of pushing to an array
+        push(@lines, $line);
+    }
+
+    close(INPUT);
+
+    foreach my $line (@lines) {
+        print($line . "\n");
+    }
 }
+
+main();
