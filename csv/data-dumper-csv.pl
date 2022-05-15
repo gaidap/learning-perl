052c33f3971437313773dd52f505729e9d918486
Paul Gaida <pc.gaida@gmail.com>
2022-05-15 16:06:47 +0200

    Parse CSV with regex.

--- csv/data-dumper-csv.pl
+++ csv/data-dumper-csv.pl
@@ -14,11 +14,26 @@ sub main {
     }
 
     <INPUT>; # Skip the first line
-    while (<INPUT>) {
+    GOTO: while (<INPUT>) {
+        # Check if line is not empty or skip it
+        $_ =~ /\S+/ or next;
+
         # Remove spaces and newlines from beginning and end of the line
         chomp $_;
 
         my @values = split(/\s*,\s*/, $_); # use regex to trim spaces on split
+
+        if(scalar(@values) < 3) {
+            print("Invalid line: $_\n");
+            next;
+        }
+
+        foreach my $line(@values) {
+            if($line eq '') {
+                print("Invalid line: $_\n");
+                next GOTO; # This is called a label and it is simply a goto. You can name it like you want.
+            }
+        }
 
         print Dumper(@values); # Print data from array to console.
     }
